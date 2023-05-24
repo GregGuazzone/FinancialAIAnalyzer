ed3325c9a2745a4609d10714a630ed862f7329d6
GregGuazzone <113939493+GregGuazzone@users.noreply.github.com>
2023-05-24 17:24:56 -0400

    refined the models and initiated create-react-app

---
+++ backend/main.py
@@ -0,0 +1,6 @@
+from models import User, Watchlist, Portfolio, Stock
+from routes import app
+
+
+if __name__ == '__main__':
+    app.run()
---
+++ backend/models.py
@@ -0,0 +1,45 @@
+from datetime import datetime
+from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy import Column, Integer, String, ForeignKey, Table
+
+db = SQLAlchemy()
+
+class User(db.Model):
+    __tablename__ = 'users'
+    id = Column(Integer, primary_key=True)
+    name = Column(String(50), nullable=False)
+    email = Column(String(50), nullable=False)
+    password = Column(String(50), nullable=False)
+    watchlists = db.relationship('Watchlist', backref='user', cascade='all, delete-orphan')
+    portfolios = db.relationship('Portfolio', backref='user', uselist=False, cascade='all, delete-orphan')
+
+class Stock(db.Model):
+    __tablename__ = 'stocks'
+    id = Column(Integer, primary_key=True)
+    symbol = Column(String(10), nullable=False)
+    name = Column(String(100), nullable=False)
+
+class Watchlist(db.Model):
+    __tablename__ = 'watchlists'
+    id = Column(Integer, primary_key=True)
+    name = Column(String(50), nullable=False)
+    user_id = Column(Integer, ForeignKey('users.id'), nullable=False)
+
+class Portfolio(db.Model):
+    __tablename__ = 'portfolios'
+    id = Column(Integer, primary_key=True)
+    name = Column(String(50), nullable=False)
+    price = Column(String(50), nullable=False)
+    quantity = Column(Integer, nullable=False)
+    user_id = Column(Integer, ForeignKey('users.id'), nullable=False)
+
+# Association tables
+watchlist_stock_association = Table('watchlist_stock_association', db.Model.metadata,
+    Column('watchlist_id', Integer, ForeignKey('watchlists.id')),
+    Column('stock_id', Integer, ForeignKey('stocks.id'))
+)
+
+portfolio_stock_association = Table('portfolio_stock_association', db.Model.metadata,
+    Column('portfolio_id', Integer, ForeignKey('portfolios.id')),
+    Column('stock_id', Integer, ForeignKey('stocks.id'))
+)
